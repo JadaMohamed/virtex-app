62268db385eefb58e58a1d672886e2ed3bd56b99
JadaMohamed <jadamohameed@gmail.com>
2023-11-27 23:10:23 +0100

    Update links in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -193,29 +193,38 @@ function App() {
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div class="sm:flex sm:items-center sm:justify-between">
             <a
-              href="https://flowbite.com/"
+              href="https://google.com"
               class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
               <img className="h-6" src={logo} class="h-8" alt="Flowbite Logo" />
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
               <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
+                <a
+                  href="https://google.com"
+                  class="hover:underline me-4 md:me-6"
+                >
                   About
                 </a>
               </li>
               <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
+                <a
+                  href="https://google.com"
+                  class="hover:underline me-4 md:me-6"
+                >
                   Privacy Policy
                 </a>
               </li>
               <li>
-                <a href="#" class="hover:underline me-4 md:me-6">
+                <a
+                  href="https://google.com"
+                  class="hover:underline me-4 md:me-6"
+                >
                   Licensing
                 </a>
               </li>
               <li>
-                <a href="#" class="hover:underline">
+                <a href="https://google.com" class="hover:underline">
                   Contact
                 </a>
               </li>
